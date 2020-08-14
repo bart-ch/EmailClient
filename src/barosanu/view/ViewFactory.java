85a8ccb738f1f66bf49f3cfe434c0693fa93b65b
Bartosz Chodyła <bartoszchodyla@gmail.com>
2020-08-15 01:12:27 +0200

    Impemented stage chaning after clicking a login button

--- src/barosanu/view/ViewFactory.java
+++ src/barosanu/view/ViewFactory.java
@@ -1,6 +1,14 @@
 package barosanu.view;
 
 import barosanu.EmailManager;
+import barosanu.controller.BaseController;
+import barosanu.controller.LoginWindowController;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+import java.io.IOException;
 
 /**
  * Created by "Bartosz Chodyla" on 2020-08-15.
@@ -15,5 +23,36 @@ public class ViewFactory {
 
     public void showLoginWindow() {
         System.out.println("Show login window called");
+
+        BaseController controller = new LoginWindowController(emailManager, this, "LoginWindow.fxml");
+        initializeStage(controller);
+    }
+
+    public void showMainWindow() {
+        System.out.println("Main window called");
+
+        BaseController controller = new LoginWindowController(emailManager, this, "MainWindow.fxml");
+        initializeStage(controller);
+    }
+
+    private void initializeStage(BaseController baseController) {
+        FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource(baseController.getFxmlName()));
+        fxmlLoader.setController(baseController);
+        Parent parent;
+        try {
+            parent = fxmlLoader.load();
+        } catch (IOException e) {
+            e.printStackTrace();
+            return;
+        }
+
+        Scene scene = new Scene(parent);
+        Stage stage = new Stage();
+        stage.setScene(scene);
+        stage.show();
+    }
+
+    public void closeStage(Stage stageToClose) {
+        stageToClose.close();
     }
 }
