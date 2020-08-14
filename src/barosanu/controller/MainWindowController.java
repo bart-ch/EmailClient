6d272b51993090963ba97dbd757bb391d110ef5b
Bartosz Chodyła <bartoszchodyla@gmail.com>
2020-08-14 17:37:11 +0200

    Added login and main window layouts

---
+++ src/barosanu/controller/MainWindowController.java
@@ -0,0 +1,26 @@
+package barosanu.controller;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.TableView;
+import javafx.scene.control.TreeView;
+import javafx.scene.web.WebView;
+
+/**
+ * Created by "Bartosz Chodyla" on 2020-08-14.
+ */
+public class MainWindowController {
+
+    @FXML
+    private TreeView<?> emailsTreeView;
+
+    @FXML
+    private TableView<?> emailsTableView;
+
+    @FXML
+    private WebView emailWebView;
+
+    @FXML
+    void optionsAction() {
+
+    }
+}
