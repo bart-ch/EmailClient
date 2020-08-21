bce526931def366d08f6b65d71f33f4c271d773e
Bartosz Chodyła <bartoszchodyla@gmail.com>
2020-08-21 12:57:47 +0200

    Implemented custom button which is changing color depending on the downloading status

--- src/bchodyla/controller/EmailDetailsController.java
+++ src/bchodyla/controller/EmailDetailsController.java
@@ -15,6 +15,8 @@ import javafx.scene.web.WebView;
 
 import javax.mail.MessagingException;
 import javax.mail.internet.MimeBodyPart;
+import java.awt.Desktop;
+import java.io.File;
 import java.net.URL;
 import java.util.ResourceBundle;
 
@@ -89,6 +91,7 @@ public class EmailDetailsController extends BaseController implements Initializa
         }
 
         private void downloadAttachment() {
+            colorBlue();
             Service service = new Service() {
                 @Override
                 protected Task createTask() {
@@ -102,6 +105,28 @@ public class EmailDetailsController extends BaseController implements Initializa
                 }
             };
             service.restart();
+            service.setOnSucceeded(e -> {
+                colorGreen();
+                this.setOnAction(e2 -> {
+                    File file = new File(downloadedFilePath);
+                    Desktop desktop = Desktop.getDesktop();
+                    if(file.exists()) {
+                        try {
+                            desktop.open(file);
+                        } catch (Exception e3) {
+                            e3.printStackTrace();
+                        }
+                    }
+                });
+            });
+        }
+        private void colorBlue() {
+            this.setStyle("-fx-background-color: Blue");
+        }
+        private void colorGreen() {
+            this.setStyle("-fx-background-color: Green");
         }
     }
+
+
 }
