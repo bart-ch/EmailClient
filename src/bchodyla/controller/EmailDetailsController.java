84a100f15ab9babf47c81272f4920d1ce65847b2
Bartosz Chodyła <bartoszchodyla@gmail.com>
2020-08-21 12:38:50 +0200

    Implemented downloading the attachment to disc

--- src/bchodyla/controller/EmailDetailsController.java
+++ src/bchodyla/controller/EmailDetailsController.java
@@ -4,12 +4,17 @@ import bchodyla.EmailManager;
 import bchodyla.controller.services.MessageRendererService;
 import bchodyla.model.EmailMessage;
 import bchodyla.view.ViewFactory;
+import javafx.concurrent.Service;
+import javafx.concurrent.Task;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.layout.HBox;
 import javafx.scene.web.WebView;
 
+import javax.mail.MessagingException;
+import javax.mail.internet.MimeBodyPart;
 import java.net.URL;
 import java.util.ResourceBundle;
 
@@ -18,6 +23,8 @@ import java.util.ResourceBundle;
  */
 public class EmailDetailsController extends BaseController implements Initializable {
 
+
+    private String LOCATION_OF_DOWNLOADS = System.getProperty("user.home") + "/Downloads/";
 
     @FXML
     private WebView webView;
@@ -46,10 +53,55 @@ public class EmailDetailsController extends BaseController implements Initializa
         EmailMessage emailMessage = emailManager.getSelectedMessage();
         subjectLabel.setText(emailMessage.getSubject());
         senderLabel.setText(emailMessage.getSender());
+        loadAttachments(emailMessage);
 
         MessageRendererService messageRendererService = new MessageRendererService(webView.getEngine());
         messageRendererService.setEmailMessage(emailMessage);
         messageRendererService.restart();
 
+    }
+
+    private void loadAttachments(EmailMessage emailMessage){
+        if (emailMessage.hasAttachments()){
+            for (MimeBodyPart mimeBodyPart: emailMessage.getAttachmentList()){
+                try {
+                    AttachmentButton button = new AttachmentButton(mimeBodyPart);
+                    hBoxDownloads.getChildren().add(button);
+                } catch (MessagingException e) {
+                    e.printStackTrace();
+                }
+            }
+        } else {
+            attachmentLabel.setText("");
+        }
+    }
+
+    private class AttachmentButton extends Button {
+        private MimeBodyPart mimeBodyPart;
+        private String downloadedFilePath;
+
+        public AttachmentButton (MimeBodyPart mimeBodyPart) throws MessagingException {
+            this.mimeBodyPart = mimeBodyPart;
+            this.setText(mimeBodyPart.getFileName());
+            this.downloadedFilePath = LOCATION_OF_DOWNLOADS + mimeBodyPart.getFileName();
+
+            this.setOnAction(e -> downloadAttachment());
+        }
+
+        private void downloadAttachment() {
+            Service service = new Service() {
+                @Override
+                protected Task createTask() {
+                    return new Task() {
+                        @Override
+                        protected Object call() throws Exception {
+                            mimeBodyPart.saveFile(downloadedFilePath);
+                            return null;
+                        }
+                    };
+                }
+            };
+            service.restart();
+        }
     }
 }
--- src/bchodyla/model/EmailMessage.java
+++ src/bchodyla/model/EmailMessage.java
@@ -35,6 +35,14 @@ public class EmailMessage {
         this.message = message;
     }
 
+    public boolean hasAttachments() {
+        return hasAttachments;
+    }
+
+    public List<MimeBodyPart> getAttachmentList() {
+        return attachmentList;
+    }
+
     public String getSubject(){
         return this.subject.get();
     }
